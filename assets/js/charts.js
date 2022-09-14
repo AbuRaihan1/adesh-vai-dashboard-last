39e9dfd13dc707e8442b90ff05c92da68354a5b5
Abu Raihan <raihanahmed01973@gmail.com>
2022-09-14 12:23:04 +0600

    Done line chart in welcome dashboard

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -37,7 +37,7 @@ const data = {
   ],
 };
 
-// config block
+// // config block
 const config = {
   type: "bar",
   data,
@@ -68,3 +68,90 @@ const config = {
 const lasWeekChart = new Chart(document.getElementById("myChart"), config);
 
 // bar chart end
+
+// welcome dashboard file start
+// line chart
+
+var ctx = document.getElementById("lineChartMain").getContext("2d");
+var gradient = ctx.createLinearGradient(0, 0, 0, 300);
+gradient.addColorStop(0, "#147AD6");
+gradient.addColorStop(1, "#FFFFFF");
+const dataLineChart = {
+  labels: ["M", "T", "W", "T", "F", "S", "S"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [20, 40, 91, 65, 68, 20, 30, 10],
+      backgroundColor: gradient,
+      pointRadius: 7,
+      borderColor: "#147AD6",
+      borderWidth: 2,
+      tension: 0.5,
+      fill: true,
+
+    },
+  ],
+};
+
+// // config block
+let configLineChart = new Chart(ctx, {
+  type: "line",
+  data: dataLineChart,
+  options: {
+    // responsive: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// const lineChartMain = new Chart(
+//   document.getElementById("lineChartMain"),
+//   configLineChart
+// );
+
+// const vissionDoughnut = new Chart(
+//   document.getElementById("lineChartMain"),
+//   configLineChart
+// );
+// let gradientBg = ctx.createLinearGradient(0,0,0,200);
+// gradientBg.addColorStop(1, 'rgba(255,99,132,1');
+
+var ctx = document.getElementById("lineChartMain").getContext("2d");
+
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        backgroundColor: gradient,
+        label: "Numbers",
+        data: [12, 19, 3, 5, 2, 3],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    tension: 0.3,
+  },
+});
