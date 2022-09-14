70d4a62ea806220faab5f25862468dc047716315
Abu Raihan <raihanahmed01973@gmail.com>
2022-09-14 22:21:15 +0600

    done percentage

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,15 +1,14 @@
 const plugin = {
-  id: 'custom_canvas_background_color',
+  id: "custom_canvas_background_color",
   beforeDraw: (chart) => {
-    const {ctx} = chart;
+    const { ctx } = chart;
     ctx.save();
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.fillStyle = 'white';
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, chart.width, chart.height);
     ctx.restore();
-  }
+  },
 };
-
 
 // bar chart start
 // setup block
@@ -53,6 +52,7 @@ const data = {
 const config = {
   type: "bar",
   data,
+  plugins: [plugin],
   options: {
     // responsive: false,
     plugins: {
@@ -80,9 +80,7 @@ const config = {
 const lasWeekChart = new Chart(document.getElementById("myChart"), config);
 // bar chart end
 
-
 // welcome dashboard file start
-
 
 // main line chart start
 var ctx = document.getElementById("lineChartMain").getContext("2d");
@@ -135,8 +133,6 @@ let configLineChart = new Chart(ctx, {
 
 // main line chart end.
 
-
-
 // lastweek line chart start
 var ctx = document.getElementById("lastWeekChart").getContext("2d");
 var gradient = ctx.createLinearGradient(0, 0, 0, 300);
@@ -188,18 +184,45 @@ let configLastWeekChart = new Chart(ctx, {
 
 // main line chart end.
 
-
-// doughunut chart garages start 
-
+// doughunut chart garages start
+const counter = {
+  id: "counter",
+  beforeDraw(Chart, args, options) {
+    const {
+      ctx,
+      chartArea: { top, right, bottom, left, width, height },
+    } = Chart;
+    ctx.save();
+    ctx.fillStyle = "#020344";
+    ctx.fillRect(width, top + height / 2, 0, 10);
+    ctx.font = "25px sans-serif";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#020344;";
+    ctx.textWeight = ctx.fillText(
+      dataDoughnut.datasets[0].data[0] + "%",
+      width + 34,
+      top + 10 + height / 2
+    );
+  },
+};
 const dataDoughnut = {
-  labels: ["Garage one", "negetive", "unsure"],
+  labels: ["Garage one", "Garage two", "Garage Three"],
   datasets: [
     {
       label: "# of Votes",
-      data: [60, 20, 6],
+      data: [54, 30, 26],
       backgroundColor: ["#147AD6", "#79D2DE", "#EC6666"],
       borderColor: ["#147AD6", "#79D2DE", "#EC6666"],
+      color: ["red", "blue", "#fff"],
       cutout: "60%",
+      // datalabels : {
+      //   color : 'black',
+      //   backgroundColor : "white",
+      //   borderRadius :50,
+      //   padding : 10,
+      //   offset : 4,
+
+      // },
     },
   ],
 };
@@ -224,6 +247,7 @@ const doughnutConfig = {
       },
     },
   },
+  // plugins : [ChartDataLabels]
 };
 
 // data redering
@@ -232,4 +256,4 @@ const garagesDoughnut = new Chart(
   doughnutConfig
 );
 
-// doughnut chart gareges end 
+// doughnut chart gareges end
