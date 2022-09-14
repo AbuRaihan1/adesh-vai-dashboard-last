6fff787764a0087c88295a5ab8f7a12cbc086d41
Abu Raihan <raihanahmed01973@gmail.com>
2022-09-14 09:51:54 +0600

    done everything

---
+++ assets/js/charts.js
@@ -0,0 +1,65 @@
+// bar chart start 
+
+// setup block 
+const data = {
+  labels: ["Jan 2018", "Feb 2018", "Mar 2019", "Apr 2019", "May 2019", "June 2019", "July 2019"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [250, 500, 750, 400, 600, 260, 400],
+        backgroundColor: [
+          "#49C0FF",
+          "#D6D6D6",
+          "#49C0FF",
+          "#D6D6D6",
+          "#49C0FF",
+          "#D6D6D6",
+        ],
+        borderColor: [
+          "#49C0FF",
+          "#D6D6D6",
+          "#49C0FF",
+          "#D6D6D6",
+          "#49C0FF",
+          "#D6D6D6",
+        ],
+        borderWidth: 1,
+        borderRadius:10,
+      },
+    ],
+}
+
+// config block 
+const config = {
+  type: "bar",
+  data,
+  options: {
+    // responsive: false,
+    plugins: {
+      legend: {
+          display: false,
+       } },
+    scales: {
+      x: {
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false
+        }
+      },
+    },
+  },
+
+}
+
+// render block 
+const lasWeekChart = new Chart(
+  document.getElementById('myChart'), config
+)
+
+
+// bar chart end 
